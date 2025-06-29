1229c537eab2ed4c1902d88c24a37450831e3eab
Markizano Draconus <markizano@markizano.net>
2025-06-29 09:37:50 -0500

    new components: candidate (for card view) and candidate detail.

---
+++ src/app/candidate/candidate-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rhf-candidate-details',
+  imports: [],
+  templateUrl: './candidate-details.component.html',
+  styles: ``
+})
+export class CandidateDetailsComponent {
+
+}
---
+++ src/app/candidate/candidate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rhf-candidate',
+  imports: [],
+  templateUrl: './candidate.component.html',
+  styles: ``
+})
+export class CandidateComponent {
+
+}
